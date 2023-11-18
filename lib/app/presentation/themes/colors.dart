aa308a5713de903328b208bf933b63c4cfa2a134
Bruno Polaski <polaskibruno03@gmail.com>
2023-11-18 19:15:15 -0300

    created home page screen, with appbar icons, body carousel and implemented repository list in homepage

---
+++ lib/app/presentation/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const white = Color(0xFFF1EAFF);
+  static const lightestPurple = Color(0xFFE5D4FF);
+  static const lightPurple = Color(0xFFDCBFFF);
+  static const purple = Color(0xFFD0A2F7);
+}
